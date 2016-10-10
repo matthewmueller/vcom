7caafdbb5241c38db6093d068327608c98dcab5a
Matthew Mueller <mattmuelle@gmail.com>
2016-10-10 20:21:25 +0700

    bump deps

--- examples/server.js
+++ examples/server.js
@@ -17,11 +17,16 @@ let css = CSS(`
 `)
 
 const App = ({ name }) => (
+  body()
   div.class('app')(
     style.type('text/css')(css()),
     h1.class(css.header)(`hi ${name}!`)
   )
 )
+
+function body () {
+
+}
 
 let html = render(App({ name: 'Matt' }))
 console.log(html)
